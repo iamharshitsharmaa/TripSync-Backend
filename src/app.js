279c6ac3f2442783d849178f0179aae093ac3e66
Harshit Sharma <harshitsharma7222@gmail.com>
2026-03-08 10:57:25 +0530

    CORS update

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: process.env.CLIENT_URL || "https://trip-sync-sigma.vercel.app",
     credentials: true,
   }),
 );
